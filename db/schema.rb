09301a68d5eee8121508beacf91381fa99546c20
Herminio Vazquez <canimus@gmail.com>
2014-03-25 09:38:35 +0800

    Addition of provider into identity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322122447) do
+ActiveRecord::Schema.define(version: 20140325012912) do
 
   create_table "identities", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140322122447) do
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
   end
 
 end
